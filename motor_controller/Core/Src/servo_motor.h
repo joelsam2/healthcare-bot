d62731d9efb53708acbb6b5312c69a9a7499feca
joelsam2 <57608717+joelsam2@users.noreply.github.com>
2021-04-19 15:58:27 -0700

    Delete motor_controller directory

--- motor_controller/Core/Src/servo_motor.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stm32f767xx.h>
-
-enum servo_motor_state{ straight = 0, hard_left, soft_left, hard_right, soft_right};
-extern enum servo_motor_state servo_motor_s;
